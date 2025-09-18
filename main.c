d775309fe84286b65449ac2c2ca29a0fe5006d89
jang-seoyun <rachel2343@naver.com>
2025-09-18 16:24:18 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,19 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	int input_int;
-	float input_float;
-	
-	printf("enter an integer : ");
-	scanf("%d", &input_int);
-	
-	
-	printf("enter a float : ");
-	scanf("%f", &input_float);
-	
-	printf("interger : %d, float : %f\n", input_int, input_float);
-	
-	system("PAUSE");
+int main(void) 
+{
+  char c;
+  int i;
+  
+  
+  printf("input a number :");
+  scanf("%c", &c);
+  
+  
+  i= c-'0';
+  printf("The input number is %i\n",i);
 	return 0;
 }
